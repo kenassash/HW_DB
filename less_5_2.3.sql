e6b3b40b2a012f628a697f8560e9bc8460eb220e
sash <kenas.sash@gmail.com>
2021-08-12 16:37:19 +0900

    test

--- less_5_2.3.sql
+++
@@ -1,6 +0,0 @@
--- Задание 3_2. Подсчитайте произведение чисел в столбце таблицы
-
-use shop;
-select * from users;
-
-select exp(sum(ln (id))) as rez from users u ;
--- less_5_2.5.sql
+++
@@ -1,9 +0,0 @@
--- Задача 5_1 Из таблицы catalogs извлекаются записи при помощи запроса. SELECT * FROM catalogs WHERE id IN (5, 1, 2); 
--- Отсортируйте записи в порядке, заданном в списке IN.
-
-
-use shop;
-
-select * from catalogs;
-
-select * from catalogs where id = 1 or id = 2 or id = 5 order by FIELD(id,5,1,2);
